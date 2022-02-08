b58fcc4b1685bc1dfcff21f56795f6378621786e
varun.raja <varun.raja@outlook.com>
2022-02-08 00:48:36 -0500

    scaffold

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta content="text/html; charset=UTF-8" name="Content-Type" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./icons/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  plugins: [],
+  theme: {},
+};
